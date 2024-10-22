f57c9414bdd0d70a2d7b34af181a8acaec140b33
SoldierBoyy <115117552+SoldierBoyy@users.noreply.github.com>
2024-10-22 10:42:55 +0300

    Update task_1.py

--- Лабораторная_работа_1/task_1.py
+++ Лабораторная_работа_1/task_1.py
@@ -1,7 +1,9 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers_without_none = numbers[:4] + numbers[5:]
+NONE_INDEX = 4
+
+numbers_without_none = numbers[:NONE_INDEX] + numbers[NONE_INDEX+1:]
 number_for_change = sum(numbers_without_none) / len(numbers)
-numbers[4] = number_for_change
+numbers[NONE_INDEX] = number_for_change
 
 print(f"Измененный список: {numbers}")
